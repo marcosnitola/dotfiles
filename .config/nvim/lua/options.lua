f4ef4b74a59cc21f9e838969ed0cd63665fff0f2
marcosnitola <marcos123.mdnr@gmail.com>
2026-02-25 14:22:25 -0500

    Neovim configuration added

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,22 @@
+vim.opt.completeopt = {'menu', 'menuone', 'noselect' }
+-- UI config
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+--vim.opt.showmode = false
+vim.opt.title = true
+vim.opt.syntax = "on"
+
+-- Tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Searching
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
