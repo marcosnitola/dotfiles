4b330c8be483a5a421252e4b11a42d6e93cac2ad
marcosnitola <marcos123.mdnr@gmail.com>
2026-08-23 16:34:31 -0500

    refactor(powermenu.sh): code cleaned

--- .config/wofi/powermenu.sh
+++ .config/wofi/powermenu.sh
@@ -1,20 +1,40 @@
 #!/usr/bin/env bash
 
-op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | wofi -i -j --style=$XDG_CONFIG_HOME/wofi/style-inputless.css --cache-file=/dev/null --dmenu | awk '{print tolower($2)}' )
+poweroff=' Poweroff'
+reboot=' Reboot'
+suspend=' Suspend'
+lock=' Lock'
+logout=' Logout'
 
-case $op in 
-        poweroff)
-                ;&
-        reboot)
-                ;&
-        suspend)
-                systemctl $op
-                ;;
-        lock)
-                swaylock
-                ;;
-        logout)
-                swaymsg exit
-                ;;
+op=$(
+  printf '%s\n' \
+    "$poweroff" \
+    "$reboot" \
+    "$suspend" \
+    "$lock" \
+    "$logout" \
+  |
+  wofi -i -j \
+    --style=$XDG_CONFIG_HOME/wofi/style-inputless.css \
+    --cache-file=/dev/null \
+    --dmenu
+)
+
+case "$op" in 
+  "$poweroff")
+    systemctl poweroff
+    ;;
+  "$reboot")
+    systemctl reboot
+    ;;
+  "$suspend")
+    systemctl suspend
+    ;;
+  "$lock")
+    swaylock
+    ;;
+  "$logout")
+    swaymsg exit
+    ;;
 esac
 
